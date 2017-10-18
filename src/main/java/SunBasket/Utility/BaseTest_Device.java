4f51d8806170955341adc94374a4ca55569bcfee
vasavi.uppada <vasavi.uppada@sunbasket.com>
2017-10-18 14:06:42 -0700

    adding BaseTest_Device

--- src/main/java/SunBasket/Utility/BaseTest_Device.java
+++ src/main/java/SunBasket/Utility/BaseTest_Device.java
@@ -1,5 +1,11 @@
 package SunBasket.Utility;
 
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest_Device extends DriverScript_Device{
 
+	@BeforeMethod
+	public void setUpDevice() {
+		
+	}
 }
